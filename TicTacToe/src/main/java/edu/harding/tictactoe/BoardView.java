f53f745de3b187c73d0ce966ec6716a2014336ba
Alejandro Velasco <savelascod@unal.edu.co>
2015-09-19 11:43:04 -0500

    -Added onDraw body

--- TicTacToe/src/main/java/edu/harding/tictactoe/BoardView.java
+++ TicTacToe/src/main/java/edu/harding/tictactoe/BoardView.java
@@ -1,7 +1,8 @@
 package edu.harding.tictactoe;
 
 import android.content.Context;
-import android.graphics.Bitmap;
+import android.graphics.*;
+import android.util.AttributeSet;
 import android.view.View;
 
 /**
@@ -13,7 +14,48 @@ public class BoardView extends View {
     private Bitmap mHumanBitmap;
     private Bitmap mAndroidBitmap;
 
+    private Paint mPaint;
+
     public BoardView(Context context) {
         super(context);
+        initialize();
     }
+
+    public BoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize();
+    }
+
+    public BoardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize();
+    }
+
+
+    public void initialize() {
+        mAndroidBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.x_img);
+        mAndroidBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.o_img);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int boardWidth = getWidth();
+        int boardHeight = getHeight();
+
+        mPaint.setColor(Color.DKGRAY);
+        mPaint.setStrokeWidth(GRID_WIDH);
+
+        int cellWidth = boardWidth / 3;
+        int cellHeight = boardHeight / 3;
+
+        canvas.drawLine(cellWidth, 0, cellWidth, boardHeight, mPaint);
+        canvas.drawLine(cellWidth * 2, 0, cellWidth * 2, boardHeight, mPaint);
+
+        canvas.drawLine(0, cellHeight, boardWidth, cellHeight, mPaint);
+        canvas.drawLine(0, cellHeight * 2, boardWidth, cellHeight * 2, mPaint);
+    }
+
 }
